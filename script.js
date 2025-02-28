f49fb1fc1a93883444ef7467c042dc9194eaf63b
luiz fellipe luquezi <102390413+HIGSHYO@users.noreply.github.com>
2025-02-28 10:51:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ document.getElementById('sidebar').addEventListener('mouseleave', function() {
     document.getElementById('arrow').style.display = 'block';
 });
 
-//muda todo o layot de cor do site
+//Muda todo o layot de cor do site
 /*Ao clicar no icone muda o icone*/
 Array.from(document.getElementsByClassName('bright')).forEach(function(element) {
     element.addEventListener('click', function() {
         const imgElement = document.getElementById('icon_bright');
         if (imgElement.src.includes('img_escuro.png')) {
             imgElement.src = 'imagens/img_claro.png';
-            document.body.style.backgroundColor = 'black';
+            document.body.style.backgroundColor = '#363636';
             document.body.style.color = 'white';
 	    document.getElementById('arrow').style.color = 'white';
         } else {
@@ -34,3 +34,19 @@ Array.from(document.getElementsByClassName('bright')).forEach(function(element) 
         }
     });
 });
+
+/* Chama as imagens da show case e as trocas a cada 3 segundos*/
+const imagens = [
+    'imagens/img_place_holder_1.png',
+    'imagens/img_place_holder_2.png',
+    'imagens/img_place_holder_3.png'
+];
+
+let indice = 0;
+
+function trocarImagem() {
+    indice = (indice + 1) % imagens.length;
+    document.getElementById('imagem_showcase').src = imagens[indice];
+}
+
+setInterval(trocarImagem, 4000);
